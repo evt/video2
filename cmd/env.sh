403defe358ad64f05829248c8c122db26c86ee62
Eugene Toropov <eugene.toropov@gmail.com>
2020-08-14 08:22:51 +0300

    More updates

--- cmd/env.sh
+++ cmd/env.sh
@@ -7,4 +7,13 @@
  export PG_USER=postgres
  export PG_PASSWORD=postgres
 
+# Google Cloud Postgres
+#export PG_PROTO=unix
+#export PG_ADDR=/cloudsql/wakeup-286404:europe-west1:wakeup-pg-master/.s.PGSQL.5432
+#export PG_DB=wakeup
+#export PG_USER=wakeup
+#export PG_PASSWORD=wakeup
+
+export GC_PROJECT=wakeup-286404
+export GC_PROJECT_LOCATION=europe-west1
 
